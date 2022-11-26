2570b07d953e3ea9a2b0b7a4757d5a047d7784df
slashr <slashr@users.noreply.github.com>
2022-11-26 01:40:03 +0100

    Terraform formatting

--- compute.tf
+++ compute.tf
@@ -96,32 +96,32 @@ resource "oci_core_instance" "arm1" {
 
 resource "oci_core_instance" "arm2" {
 
-    availability_domain = data.oci_identity_availability_domains.availability_domains.availability_domains[0].name
-    compartment_id = var.compartment_id
-    shape = "VM.Standard.A1.Flex"
-    display_name = "arm2"
+  availability_domain = data.oci_identity_availability_domains.availability_domains.availability_domains[0].name
+  compartment_id      = var.compartment_id
+  shape               = "VM.Standard.A1.Flex"
+  display_name        = "arm2"
 
-    shape_config {
+  shape_config {
     memory_in_gbs = 12
     ocpus         = 2
-    }
+  }
 
-    metadata = {
+  metadata = {
     ssh_authorized_keys = var.ssh_authorized_keys,
     #user_data = filebase64("${path.module}/scripts/init.sh")
-    }
+  }
 
-    create_vnic_details {
+  create_vnic_details {
     assign_public_ip          = true
     subnet_id                 = oci_core_subnet.public_subnet.id
     assign_private_dns_record = true
     private_ip                = "10.0.0.40"
     hostname_label            = "arm2"
-    }
+  }
 
-    source_details {
+  source_details {
     #Required
-    source_id = var.ampere_source_image_id
+    source_id   = var.ampere_source_image_id
     source_type = "image"
-    }
+  }
 }

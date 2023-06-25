613ad2fcde7dc7d04e0797cefe6be9a142a2497c
slashr <slashr@users.noreply.github.com>
2023-06-25 02:02:41 +0200

    Remove sa

--- gcp/compute.tf
+++ gcp/compute.tf
@@ -19,10 +19,4 @@ resource "google_compute_instance" "gcp1" {
       // Ephemeral public IP
     }
   }
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
 }

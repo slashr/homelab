eec092e4e72d18ed882af03b70fd49334940d717
slashr <slashr@users.noreply.github.com>
2023-08-26 16:25:46 +0200

    also disable metallb

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -7,9 +7,9 @@ module "argo-cd" {
 #   source = "git::https://github.com/slashr/terraform-modules.git//apps/ingress-nginx?ref=main"
 # }
 
-module "metallb" {
-  source = "git::https://github.com/slashr/terraform-modules.git//apps/metallb?ref=main"
-}
+# module "metallb" {
+#  source = "git::https://github.com/slashr/terraform-modules.git//apps/metallb?ref=main"
+# }
 
 module "cert-manager" {
   source               = "git::https://github.com/slashr/terraform-modules.git//apps/cert-manager?ref=main"

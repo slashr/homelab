90f41c0c4c110c73da956a71c49b1823a6c968dc
slashr <slashr@users.noreply.github.com>
2023-12-10 18:02:16 +0100

    install externaldns

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -6,3 +6,7 @@ module "cert-manager" {
   source               = "git::https://github.com/slashr/terraform-modules.git//apps/cert-manager?ref=main"
   cloudflare_api_token = var.cloudflare_api_token
 }
+
+module "external-dns" {
+  source               = "git::https://github.com/slashr/terraform-modules.git//apps/external-dns?ref=main"
+}

9b66b443818a28b506b1f6556b773f6bfc22edd1
slashr <slashr@users.noreply.github.com>
2024-04-22 20:34:11 +0200

    add tf modules

---
+++ modules/ingress-nginx/ingress-nginx.tf
@@ -0,0 +1,17 @@
+resource "kubernetes_namespace" "ingress-nginx" {
+  metadata {
+    name = "ingress-nginx"
+  }
+}
+
+resource "helm_release" "ingress-nginx" {
+  name       = "ingress-nginx"
+  namespace  = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "~4.7.1"
+
+  depends_on = [
+    resource.kubernetes_namespace.ingress-nginx
+  ]
+}

cce6788f87930b4e3812e837979a92bbd6514656
slashr <slashr@users.noreply.github.com>
2024-04-22 20:34:11 +0200

    add tf modules

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -1,13 +1,13 @@
 module "argo-cd" {
-  source = "git::https://github.com/slashr/terraform-modules.git//apps/argo-cd?ref=main"
+  source = "../modules/argo-cd"
 }
 
 module "cert-manager" {
-  source               = "git::https://github.com/slashr/terraform-modules.git//apps/cert-manager?ref=main"
+  source               = "../modules/cert-manager"
   cloudflare_api_token = var.cloudflare_api_token
 }
 
 module "external-dns" {
-  source               = "git::https://github.com/slashr/terraform-modules.git//apps/external-dns?ref=main"
+  source               = "../modules/external-dns"
   cloudflare_api_token = var.cloudflare_api_token
 }

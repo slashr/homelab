06beff91c49b7d4a37144fc760df727990c24c0c
slashr <slashr@users.noreply.github.com>
2025-04-03 19:18:43 +0200

    improve tf code

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -1,13 +1,25 @@
 module "argo-cd" {
   source = "../terraform-modules/argo-cd"
+  depends_on = [module.cert-manager, module.external-dns]
 }
 
 module "cert-manager" {
   source               = "../terraform-modules/cert-manager"
   cloudflare_api_token = var.cloudflare_api_token
+  
+  # Ensure cert-manager is deployed first as it's often a dependency
+  count = 1
 }
 
 module "external-dns" {
   source               = "../terraform-modules/external-dns"
   cloudflare_api_token = var.cloudflare_api_token
+  depends_on = [module.cert-manager]
+}
+
+# Add outputs for important information
+output "argocd_endpoint" {
+  value       = module.argo-cd.endpoint
+  description = "ArgoCD endpoint URL"
+  sensitive   = false
 }

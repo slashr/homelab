9fb106fadafd7c2caa5b5614aee9b8bc4d19ed60
slashr <slashr@users.noreply.github.com>
2025-04-03 19:26:16 +0200

    formatting

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -1,12 +1,12 @@
 module "argo-cd" {
-  source = "../terraform-modules/argo-cd"
+  source     = "../terraform-modules/argo-cd"
   depends_on = [module.cert-manager, module.external-dns]
 }
 
 module "cert-manager" {
   source               = "../terraform-modules/cert-manager"
   cloudflare_api_token = var.cloudflare_api_token
-  
+
   # Ensure cert-manager is deployed first as it's often a dependency
   count = 1
 }
@@ -14,7 +14,7 @@ module "cert-manager" {
 module "external-dns" {
   source               = "../terraform-modules/external-dns"
   cloudflare_api_token = var.cloudflare_api_token
-  depends_on = [module.cert-manager]
+  depends_on           = [module.cert-manager]
 }
 
 # Add outputs for important information

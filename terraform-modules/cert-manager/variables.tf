b3f791ca16a5ca3091987473b8896c2109a98370
slashr <slashr@users.noreply.github.com>
2025-06-21 11:50:14 +0200

    upgrade cert-manager

--- terraform-modules/cert-manager/variables.tf
+++ terraform-modules/cert-manager/variables.tf
@@ -7,7 +7,7 @@ variable "namespace" {
 variable "chart_version" {
   description = "The chart version to install. (Should match the cert-manager crd release version.)"
   type        = string
-  default     = "1.12.3"
+  default     = "1.13.2"
 }
 
 variable "timeout" {

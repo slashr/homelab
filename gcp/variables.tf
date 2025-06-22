6a72cedbceabeba1e4a3b0b96e4475a63718792d
Akash Nair <slashr@users.noreply.github.com>
2025-06-22 11:08:13 +0200

    Add variables for SSH user and key

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -22,3 +22,15 @@ variable "machine_type" {
   default     = "e2-micro"
 }
 
+variable "ssh_username" {
+  description = "Username for SSH login"
+  type        = string
+  default     = "dev"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for the instance"
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ5Ysv6PF3HbWQ/JfP2vWEBHtH8wPv6ysbyosEREXpO3"
+}
+

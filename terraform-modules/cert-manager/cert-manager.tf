e4c9c6011dc18b2cb4f40d5294a1281ed73d8f0a
slashr <slashr@users.noreply.github.com>
2025-12-21 14:00:02 +0100

    fix: use kubernetes_secret_v1 for cert-manager

--- terraform-modules/cert-manager/cert-manager.tf
+++ terraform-modules/cert-manager/cert-manager.tf
@@ -34,12 +34,12 @@ resource "kubernetes_manifest" "letsencrypt_prod_clusterissuer" {
 
   depends_on = [
     helm_release.cert-manager,
-    kubernetes_secret.cloudflare-api-token,
+    kubernetes_secret_v1.cloudflare-api-token,
   ]
 }
 
 # Required for cert-manager to allow it to set dns records on cloudflare
-resource "kubernetes_secret" "cloudflare-api-token" {
+resource "kubernetes_secret_v1" "cloudflare-api-token" {
   metadata {
     name      = "cloudflare-api-token-secret"
     namespace = "cert-manager"
